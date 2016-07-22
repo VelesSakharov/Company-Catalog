1160212b8f47130bef59cf5a42332d1b143e2488
Veles Sakh <velessakharov@gmail.com>
2016-07-22 15:55:51 +0300

    working creation of companies

--- app/models/company_creation.rb
+++ app/models/company_creation.rb
@@ -1,26 +1,2 @@
-class Company_Creation < ApplicationRecord
-
-  def new
-  #initial
-  end
-
-  #create
-  def create
-  puts "in db"
-  end
-  #read
-  def show
-  puts "from db"
-  end
-
-  #update
-  def update
-  puts "in \ from db"
-  end
-
-  #delete
-  def destroy
-    puts "out of db"
-  end
-
+class Company_Creation
 end
